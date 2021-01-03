33b94ea807dc65e0a7eade028ef852ee5792e5be
Connie <leungcnie@gmail.com>
2021-01-03 05:14:13 +0000

    Add logic to determine winner

--- main.rb
+++ main.rb
@@ -1,3 +1,22 @@
 require './player'
 require './question'
 require './turn'
+
+p1 = Player.new(3)
+p2 = Player.new(0)
+
+while p1.score != 0 && p2.score != 0
+
+end
+
+if p1.score == 0
+  winner = 'Player 2'
+  score = p2.score
+elsif p2.score == 0
+  winner = 'Player 1'
+  score = p1.score
+end
+
+puts "#{winner} wins with a score of #{score}/3"
+puts "----- GAME OVER -----"
+puts "Good bye!"
