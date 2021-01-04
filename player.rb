bf9b806d71bb4d831a8d298a8f3602adde9ca73a
Connie <leungcnie@gmail.com>
2021-01-04 20:18:52 +0000

    Rename variables and add methods for adding a life and subtracting a life

--- player.rb
+++ player.rb
@@ -1,8 +1,21 @@
 class Player
-  attr_accessor :score
+  attr_accessor :lives, :name
 
-  def initialize(score)
-    @score = score
+  def initialize(lives, name)
+    @lives = lives
+    @name = name
+  end
+
+  def add_life
+    if @lives < 3
+      @lives += 1
+    end
+  end
+
+  def minus_life
+    if @lives > 0
+      @lives -= 1
+    end
   end
 
 end
