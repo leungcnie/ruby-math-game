9d86846d1e0b64114b36e00a0546149cebcedb61
Connie <leungcnie@gmail.com>
2021-01-04 20:21:16 +0000

    Clean up test code and initialize players and game

--- main.rb
+++ main.rb
@@ -1,22 +1,11 @@
 require './player'
 require './question'
 require './turn'
+require './game'
 
-p1 = Player.new(3)
-p2 = Player.new(0)
+p1 = Player.new(3, "Player 1")
+p2 = Player.new(3, "Player 2")
 
-while p1.score != 0 && p2.score != 0
-
-end
-
-if p1.score == 0
-  winner = 'Player 2'
-  score = p2.score
-elsif p2.score == 0
-  winner = 'Player 1'
-  score = p1.score
-end
-
-puts "#{winner} wins with a score of #{score}/3"
-puts "----- GAME OVER -----"
-puts "Good bye!"
+game = Game.new(p1, p2)
+game.start_game
+game.end_game
